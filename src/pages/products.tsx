d087e28778c068a3d4d2586e31a760fa287e97ee
Dato Rogava <drogava@crocobet.local>
2024-12-20 05:03:06 -0800

    a

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,6 +1,7 @@
 
 'use client'
 import axios from "axios"
+import styles from "@/styles/Home.module.css"
 import { useEffect,useState } from "react"
 
 export default () => {
@@ -8,26 +9,31 @@ export default () => {
     const [sort,setSort] = useState('asc')
     const [limit, setLimit ] = useState('20')
     const [loading,setLoading] = useState(false)
-    useEffect(() => {
-        const funq = async () => {
-            await axios.get(`https://fakestoreapi.com/products?limit=${limit}&sort=${sort}`).then((response) => {
-                setData(response.data)
-            })
-            setLoading(true);
-        }
-        funq();
-    },[])
+
+    const funqcia = () => {
+        axios.get(`https://fakestoreapi.com/products?limit=${limit}&sort=${sort}`).then((response) => {
+           setData(response.data)
+        })
+        setLoading(true)
+    }
+    useEffect(funqcia,[sort,limit])
     return (
         <div>
             {!loading && <p style={{color: 'white'}}>Loading...</p>}
+            <input type="input" value={limit} onChange={(e) => {setLimit(e.target.value)}} />
+            <select value={sort} onChange={(e) => {setSort(e.target.value)}}>
+                <option>asc</option>
+                <option>desc</option>
+            </select>
             <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'flex-start', alignItems: 'flex-start', marginTop: '50px', marginLeft: '70px'}}>
             {
             data.map((item: any,index) => {
                 return(
-                    <div style={{marginLeft: '40px'}} key={index}>
+                    <div style={{
+                        marginLeft: '40px'}} key={index}>
                         <h3 style={{color: 'white', width: '200px', cursor: "pointer", whiteSpace: 'nowrap', textOverflow: 'ellipsis', overflow: 'hidden'}}>{item.title}</h3>
                         <h5  style={{color: 'white'}}>{item.price}$</h5>
-                        <img style={{width: '250px', height: '250px', cursor: "pointer"}}  src={item.image}/>
+                        <img style={{width: '250px', height: '250px'}}  src={item.image}/>
                     </div>
                     )
                 })
